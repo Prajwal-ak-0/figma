22b0d7a795eca39a86a3dad5af686b8af76b941a
Prajwal A K <prajwalhassan7@gmail.com>
2024-03-02 00:28:27 +0530

    Navigation Completed

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "./_components/Navbar";
 import { OrgSidebar } from "./_components/OrgSidebar";
 import { Sidebar } from "./_components/sidebar/Index";
 
@@ -10,11 +11,12 @@ const DashboardLayout = ({
 }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
-        <Sidebar/>
+      <Sidebar />
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
-          <OrgSidebar/>
+          <OrgSidebar />
           <div className="h-full flex-1">
+            <Navbar />
             {children}
           </div>
         </div>
